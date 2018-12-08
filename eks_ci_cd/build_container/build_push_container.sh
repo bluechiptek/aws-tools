54b84233c82db51b14a26d63cb4a7602ab48ca9d
NimbusScale <joe@nimbusscale.com>
2018-12-08 09:47:09 -0800

    build container

---
+++ eks_ci_cd/build_container/build_push_container.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]] ; do
+    key="$1"
+    case $key in
+        --name)
+        NAME="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --param-key)
+        PARAM_KEY="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --tag)
+        TAG="$2"
+        shift # past argument
+        shift # past value
+        ;;
+    esac
+done
+
+if [ -z ${NAME} ] ; then
+    NAME="b4cs-build"
+fi
+
+if [ -z ${TAG} ] ; then
+    TAG="latest"
+fi
+
+if [ -z ${PARAM_KEY} ] ; then
+    PARAM_KEY="/build/codebuild/image"
+fi
+
+REGION=$(aws configure get region)
+ACCOUNT_ID=$(aws sts get-caller-identity --output text | awk '{print $1}')
+CONTAINER_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${NAME}:${TAG}"
+
+###
+# ECR repo for container needs to exist
+###
+echo "Building and pushing container ${CONTAINER_URL}"
+$(aws ecr get-login --no-include-email --region us-west-2)
+docker build -t ${CONTAINER_URL} .
+docker push ${CONTAINER_URL}
+
+echo "Updating param ${PARAM_KEY} with ${CONTAINER_URL}"
+aws ssm put-parameter --name ${PARAM_KEY} --type String --value ${CONTAINER_URL} --overwrite
+
---
+++ eks_ci_cd/build_container/build_scripts/gen-tag.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]] ; do
+    key="$1"
+    case ${key} in
+        --container)
+        TAG_TYPE=container
+        shift
+        ;;
+        --chart)
+        TAG_TYPE=chart
+        shift
+        ;;
+        --latest)
+        TAG_TYPE=latest
+        shift
+        ;;
+    esac
+done
+
+CURRENT_SHA=$(git rev-parse HEAD)
+CURRENT_SHA_SHORT=$(git rev-parse --short=8 HEAD)
+LATEST_TAG=$(git describe --abbrev=0 --tags)
+TAG_SHA=$(git show ${LATEST_TAG} -s --format=format:%H)
+
+
+if [ ${CURRENT_SHA} != ${TAG_SHA} ] ; then
+    case ${TAG_TYPE} in
+        container)
+        TAG="dev-${CURRENT_SHA_SHORT}"
+        ;;
+        chart)
+        TAG="${LATEST_TAG}+${CURRENT_SHA_SHORT}"
+        ;;
+        latest)
+        TAG="false"
+        ;;
+    esac
+else
+    case ${TAG_TYPE} in
+        latest)
+        TAG="true"
+        ;;
+        *)
+        TAG="${LATEST_TAG}"
+        ;;
+    esac
+
+fi
+
+echo ${TAG}
